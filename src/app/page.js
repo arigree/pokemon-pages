1c72319eec8566f0b8d1f3a2ad4452801fdb6d15
arigree <arigree@iu.edu>
2024-10-30 09:44:00 -0400

    troubleshooting

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,27 @@
 "use client";
-import usePokemonApi from "@/hooks/usePokemonApi"
+import usePokemonApi from "@/hooks/usePokemonApi";
 import { useEffect } from "react";
-import homeStyles from "@/app/page.module.css"
-import PokemonCard from "@/components/Pokemon/PokemonCard"
+import homeStyles from "./page.module.css";
+import PokemonCard from "@/components/Pokemon/PokemonCard";
 
-export default function Home(){
+export default function Home() {
   const pokeData = usePokemonApi();
-  
+
   useEffect(() => {
-    if(pokeData.totalPokemonCount === 0){
+    if (pokeData.totalPokemonCount === 0) {
       pokeData.getNumberOfPokemon();
     }
   }, [pokeData]);
-  console.log(pokeData)
- return(
-  <main className={homeStyles.mainContent}>
-    <h1>POKEMON SHOWCASE</h1>
-    <section>
-      <PokemonCard/>
-      <PokemonCard/>
-      <PokemonCard/>
-    </section>
-  </main>
- )
+
+  console.log(pokeData);
+  return (
+    <main className={homeStyles.mainContent}>
+      <h1>POKEMON SHOWCASE</h1>
+      <section>
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+      </section>
+    </main>
+  );
 }
